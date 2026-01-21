dd4ce3dce33e1cd65bef95118083c45718bd1c4d
Tomasz Łucarz <tomasz.lucarz@gmail.com>
2026-01-21 15:58:11 +0100

    run tutorial06_step_setup_table.feature

---
+++ features/steps/testutil.py
@@ -0,0 +1,20 @@
+# file:features/steps/testutil.py
+# ----------------------------------------------------------------------------
+# TEST SUPPORT:
+# ----------------------------------------------------------------------------
+class NamedNumber(object):
+    """Map named numbers into numbers."""
+    MAP = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+    }
+
+    @classmethod
+    def from_string(cls, named_number):
+        name = named_number.strip().lower()
+        return cls.MAP[name]
