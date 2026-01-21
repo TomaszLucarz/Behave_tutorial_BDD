a37aab8e3e9dbfd735973d9707df6fdf232d93d3
Tomasz Łucarz <tomasz.lucarz@gmail.com>
2026-01-21 17:05:54 +0100

    run tutorial09_background.feature

--- features/steps/step_tutorial02.py
+++ features/steps/step_tutorial02.py
@@ -21,6 +21,7 @@ Feature: Fight or Flight (Natural Language)
         Then the ninja should run for his life
 """
 
+
 # @mark.domain_model
 # ----------------------------------------------------------------------------
 # PROBLEM DOMAIN:
@@ -29,6 +30,7 @@ class NinjaFight(object):
     """
     Domain model for ninja fights.
     """
+
     # pylint: disable=R0903
 
     def __init__(self, with_ninja_level=None):
@@ -48,25 +50,45 @@ class NinjaFight(object):
         else:
             return "run for his life"
 
+
 # @mark.steps
 # ----------------------------------------------------------------------------
 # STEPS:
 # ----------------------------------------------------------------------------
-from behave   import given, when, then
+from behave import given, when, then
 from hamcrest import assert_that, equal_to, is_not
+
 
 @given('the ninja has a {achievement_level}')
 def step_the_ninja_has_a(context, achievement_level):
     context.ninja_fight = NinjaFight(achievement_level)
 
+
 @when('attacked by a {opponent_role}')
 def step_attacked_by_a(context, opponent_role):
     context.ninja_fight.opponent = opponent_role
+
 
 @when('attacked by {opponent}')
 def step_attacked_by(context, opponent):
     context.ninja_fight.opponent = opponent
 
+
 @then('the ninja should {reaction}')
 def step_the_ninja_should(context, reaction):
     assert_that(reaction, equal_to(context.ninja_fight.decision()))
+
+
+# ----------------------------------------------------------------------------
+# BACKGROUND-STEPS: Needed for tutorial09
+# ----------------------------------------------------------------------------
+@given('the ninja encounters another opponent')
+def step_the_ninja_encounters_another_opponent(context):
+    """
+    BACKGROUND steps are called at begin of each scenario before other steps.
+    """
+    # -- SETUP/TEARDOWN:
+    if hasattr(context, "ninja_fight"):
+        # -- VERIFY: Double-call does not occur.
+        assert_that(context.ninja_fight, is_not(equal_to(None)))
+    context.ninja_fight = None
---
+++ features/tutorial09_background.feature
@@ -0,0 +1,16 @@
+# file:features/tutorial09_background.feature
+Feature: Using Background -- Fight or Flight (Natural Language Part2, tutorial09)
+
+  # Use the Background concept to execute a number of steps before each scenario.
+  Background: Ninja fight setup
+    Given the ninja encounters another opponent
+
+  Scenario: Weaker opponent
+    Given the ninja has a third level black-belt
+    When attacked by a samurai
+    Then the ninja should engage the opponent
+
+  Scenario: Stronger opponent
+    Given the ninja has a second level black-belt
+    When attacked by Chuck Norris
+    Then the ninja should run for his life
